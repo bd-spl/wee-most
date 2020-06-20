f603e65372c9d091a4bd0143bec9f2e03cd21e9c
Reed Wade <reedwade@misterbanal.net>
2020-06-20 17:50:33 +0200

    add missing id to channels

--- wee_matter/room.py
+++ wee_matter/room.py
@@ -5,4 +5,5 @@ def create_room(data, server):
     buffer = weechat.buffer_new(data["display_name"], "", "", "", "")
 
     weechat.buffer_set(buffer, "server_name", server.name)
+    weechat.buffer_set(buffer, "channel_id", data["id"])
 
