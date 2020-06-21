4428dc9a02308a905715e069dfab40e1bd04a37e
Reed Wade <reedwade@misterbanal.net>
2020-06-21 12:22:13 +0200

    add post

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from wee_matter.server import (connect_server_cb, connect_server_teams_cb,
                                connect_server_team_channels_cb, disconnect_server_cb,
                                connect_server_users_cb, auto_connect, disconnect_all)
 
-from wee_matter.room import (hidrate_room_cb, hidrate_room_users_cb)
+from wee_matter.room import (hidrate_room_cb, hidrate_room_users_cb,
+                             room_input_cb, post_post_cb)
 
 WEECHAT_SCRIPT_NAME = "wee-matter"
 WEECHAT_SCRIPT_DESCRIPTION = "mattermost chat plugin"
--- wee_matter/room.py
+++ wee_matter/room.py
@@ -3,6 +3,37 @@ import weechat
 import json
 from wee_matter.server import server_root_url, get_server
 
+def post_post_cb(buffer, command, rc, out, err):
+    if rc != 0:
+        weechat.prnt(buffer, "Can't send post")
+        return weechat.WEECHAT_RC_ERROR
+
+    return weechat.WEECHAT_RC_OK
+
+def room_input_cb(data, buffer, input_data):
+    server_name = weechat.buffer_get_string(buffer, "localvar_server_name")
+    server = get_server(server_name)
+
+    url = server_root_url(server) + "/api/v4/posts"
+    params = {
+        "channel_id": weechat.buffer_get_string(buffer, "localvar_channel_id"),
+        "message": input_data,
+    }
+
+    weechat.hook_process_hashtable(
+        "url:" + url,
+        {
+            "failonerror": "1",
+            "httpheader": "Authorization: Bearer " + server.user_token,
+            "postfields": json.dumps(params),
+        },
+        30 * 1000,
+        "post_post_cb",
+        buffer
+    )
+    return weechat.WEECHAT_RC_OK
+
+
 def write_post(buffer, username, message, date):
     weechat.prnt_date_tags(buffer, date, "", username + "	" + message)
 
@@ -58,7 +89,7 @@ def create_room(data, server):
     if "" == room_name:
         return
 
-    buffer = weechat.buffer_new(room_name, "", "", "", "")
+    buffer = weechat.buffer_new(room_name, "room_input_cb", "", "", "")
 
     weechat.buffer_set(buffer, "localvar_set_server_name", server.name)
     weechat.buffer_set(buffer, "localvar_set_channel_id", data["id"])
