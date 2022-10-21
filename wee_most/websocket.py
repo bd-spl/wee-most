24fc5a5582858e2852dc624a6904aeb8f1900c08
Damien Tardy-Panis <damien.dev@tardypad.me>
2022-10-21 09:26:01 +0200

    Simplify all websocket event handle functions
    Send data and broadcast as parameters instead of accessing them inside
    each handler

--- wee_most/websocket.py
+++ wee_most/websocket.py
@@ -93,10 +93,8 @@ def ws_ping_cb(server_id, remaining_calls):
 
     return weechat.WEECHAT_RC_OK
 
-def handle_posted_message(server, message):
-    data = message["data"]
+def handle_posted_message(server, data, broadcast):
     post = json.loads(data["post"])
-    broadcast = message["broadcast"]
 
     if data["team_id"] and data["team_id"] not in server.teams:
         return
@@ -110,52 +108,38 @@ def handle_posted_message(server, message):
     if post.buffer == weechat.current_buffer():
         post.channel.mark_as_read()
 
-def handle_reaction_added_message(server, message):
-    data = message["data"]
-
+def handle_reaction_added_message(server, data, broadcast):
     reaction_data = json.loads(data["reaction"])
 
     reaction = wee_most.post.Reaction(server, **reaction_data)
 
     wee_most.post.add_reaction_to_post(reaction)
 
-def handle_reaction_removed_message(server, message):
-    data = message["data"]
-
+def handle_reaction_removed_message(server, data, broadcast):
     reaction_data = json.loads(data["reaction"])
 
     reaction = wee_most.post.Reaction(server, **reaction_data)
 
     wee_most.post.remove_reaction_from_post(reaction)
 
-def handle_post_edited_message(server, message):
-    data = message["data"]
-
+def handle_post_edited_message(server, data, broadcast):
     post_data = json.loads(data["post"])
     post = wee_most.post.Post(server, **post_data)
     wee_most.post.write_post_edited(post)
 
-def handle_post_deleted_message(server, message):
-    data = message["data"]
-
+def handle_post_deleted_message(server, data, broadcast):
     post_data = json.loads(data["post"])
     post = wee_most.post.Post(server, **post_data)
     wee_most.post.write_post_deleted(post)
 
-def handle_channel_created_message(server, message):
-    data = message["data"]
-
+def handle_channel_created_message(server, data, broadcast):
     wee_most.server.connect_server_team_channel(data["channel_id"], server)
 
-def handle_channel_updated_message(server, message):
-    data = message["data"]
-
+def handle_channel_updated_message(server, data, broadcast):
     channel_data = json.loads(data["channel"])
     wee_most.channel.set_channel_properties_from_channel_data(channel_data, server)
 
-def handle_channel_viewed_message(server, message):
-    data = message["data"]
-
+def handle_channel_viewed_message(server, data, broadcast):
     buffer = server.get_channel(data["channel_id"]).buffer
     if buffer:
         weechat.buffer_set(buffer, "unread", "-")
@@ -164,48 +148,39 @@ def handle_channel_viewed_message(server, message):
         last_post_id = weechat.buffer_get_string(buffer, "localvar_last_post_id")
         weechat.buffer_set(buffer, "localvar_set_last_read_post_id", last_post_id)
 
-def handle_user_added_message(server, message):
-    data = message["data"]
-    broadcast = message["broadcast"]
-
+def handle_user_added_message(server, data, broadcast):
     if data["user_id"] == server.me.id: # we are geing invited
         wee_most.server.connect_server_team_channel(broadcast["channel_id"], server)
     else:
         channel = server.get_channel(broadcast["channel_id"])
         channel.add_user(data["user_id"])
 
-def handle_channel_added_message(server, message):
-    broadcast = message["broadcast"]
+def handle_channel_added_message(server, data, broadcast):
     wee_most.server.connect_server_team_channel(broadcast["channel_id"], server)
 
-def handle_group_added_message(server, message):
+def handle_group_added_message(server, data, broadcast):
     handle_channel_added_message(server, message)
 
-def handle_direct_added_message(server, message):
+def handle_direct_added_message(server, data, broadcast):
     handle_channel_added_message(server, message)
 
-def handle_group_added_message(server, message):
+def handle_group_added_message(server, data, broadcast):
     handle_channel_added_message(server, message)
 
-def handle_new_user_message(server, message):
+def handle_new_user_message(server, data, broadcast):
     user_id = message["data"]["user_id"]
     wee_most.http.enqueue_request(
         "run_get_user",
         server, user_id, "new_user_cb", server.id
     )
 
-def handle_user_removed_message(server, message):
-    data = message["data"]
-    broadcast = message["broadcast"]
-
+def handle_user_removed_message(server, data, broadcast):
     if broadcast["channel_id"]:
         user = server.users[data["user_id"]]
         buffer = server.get_channel(broadcast["channel_id"]).buffer
         wee_most.channel.remove_channel_user(buffer, user)
 
-def handle_added_to_team_message(server, message):
-    data = message["data"]
-
+def handle_added_to_team_message(server, data, broadcast):
     user = server.users[data["user_id"]]
 
     server.teams[data["team_id"]] = None
@@ -215,16 +190,12 @@ def handle_added_to_team_message(server, message):
         data["team_id"], server, "connect_server_team_cb", server.id
     )
 
-def handle_leave_team_message(server, message):
-    data = message["data"]
-
+def handle_leave_team_message(server, data, broadcast):
     user = server.users[data["user_id"]]
     team = server.teams.pop(data["team_id"])
     team.unload()
 
-def handle_status_change_message(server, message):
-    data = message["data"]
-
+def handle_status_change_message(server, data, broadcast):
     # this event seems only to be triggered on own user
     user_id = data["user_id"]
 
@@ -245,7 +216,7 @@ def handle_ws_event_message(server, message):
     if handler_function_name not in globals():
         return
 
-    globals()[handler_function_name](server, message)
+    globals()[handler_function_name](server, message["data"], message["broadcast"])
 
 def handle_ws_message(server, message):
     if "event" in message:
